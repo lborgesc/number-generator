8200bbe8427cb146f991598a65bae0c486cff75e
lborgesc <lborgescaetano@gmail.com>
2024-04-10 22:40:28 -0300

    Validating quantity

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-function sortear( ) {
-    const amount = parseInt( document.getElementById( "quantidade" ).value );
-    const min = parseInt( document.getElementById( "de" ).value );
-    const max = parseInt( document.getElementById( "ate" ).value );
+function sortear() {
+    const amount = parseInt(document.getElementById("quantidade").value);
+    const min = parseInt(document.getElementById("de").value);
+    const max = parseInt(document.getElementById("ate").value);
 
-    if ( !amount || !min || !max ) {
-        alert( "preenchar todos os dado" );
+    if (!amount || !min || !max) {
+        alert("Preenchar todos os dado");
+        return;
+    }
+
+    if ( amount > max ) {
+        alert("Quantidade inválida");
         return;
     }
 
     const sweepstake = [];
 
-    for ( let i = 0; i < amount; i++ ) { 
-        sweepstake.push(  Math.floor( Math.random( ) * ( max - min + 1 ) + min ) );
-    }    
+    for (let i = 0; i < amount; i++) {
+        let result;
+        result = Math.floor(Math.random() * (max - min + 1) + min)
 
-    document.querySelector( "div[id='resultado'] > label" ).innerHTML = `Números sorteados: ${sweepstake}`
-    document.getElementById( "btn-reiniciar" ).setAttribute( "class", "container__botao" )
+        while (sweepstake.includes(result)) {
+            result = Math.floor(Math.random() * (max - min + 1) + min)
+        }
+
+        sweepstake.push(result);
+    }
+
+    document.querySelector("div[id='resultado'] > label").innerHTML = `Números sorteados: ${sweepstake}`
+    document.getElementById("btn-reiniciar").setAttribute("class", "container__botao")
 }
 
-function reiniciar( ) {
+function reiniciar() {
 
-    if ( document.getElementById( "btn-reiniciar" ).getAttribute( "class" ) === "container__botao-desabilitado" ) return;
+    if (document.getElementById("btn-reiniciar").getAttribute("class") === "container__botao-desabilitado") return;
 
-    const amount = parseInt( document.getElementById( "quantidade" ).value = "" );
-    const min = parseInt( document.getElementById( "de" ).value = "" );
-    const max = parseInt( document.getElementById( "ate" ).value = "" );
+    const amount = parseInt(document.getElementById("quantidade").value = "");
+    const min = parseInt(document.getElementById("de").value = "");
+    const max = parseInt(document.getElementById("ate").value = "");
 
-    document.querySelector( "div[id='resultado'] > label" ).innerHTML = `Números sorteados:  nenhum até agora`
-    document.getElementById( "btn-reiniciar" ).setAttribute( "class", "container__botao-desabilitado" )
+    document.querySelector("div[id='resultado'] > label").innerHTML = `Números sorteados:  nenhum até agora`
+    document.getElementById("btn-reiniciar").setAttribute("class", "container__botao-desabilitado")
 }
+
